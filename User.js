79b2e2c9ce643e7961e37327a8307c5f3458f410
JurrijnP <slubadog@gmail.com>
2017-10-04 17:39:59 +0200

    Add files via upload

---
+++ User.js
@@ -0,0 +1,23 @@
+function GetRoleName(Id) {
+  var Role = "";
+  for (var i = 0; i < ServerRoles.length; i++) {
+    if (ServerRoles[i].id === Id) {
+      Role = ServerRoles[i].name;
+    };
+  };
+  return Role;
+};
+
+function GetUsername(Id) {
+	var msg = "";
+	for (var i = 0; i < Object.keys(ServerMembers).length; i++) {
+		if (ServerMembers[i]["User"]["ID"] === Id) {
+			if (ServerMembers[i]["Nick"] !== "") {
+				msg = ServerMembers[i]["Nick"];
+			} else {
+				msg = ServerMembers[i]["User"]["Username"];
+			};
+		};
+	};
+	return msg;
+};
---
+++ VariableManipulation.js
@@ -0,0 +1,46 @@
+function sortObject(o) {
+    var sorted = {},
+    key, a = [];
+
+    for (key in o) {
+        if (o.hasOwnProperty(key)) {
+            a.push(key);
+        }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+        sorted[a[key]] = o[a[key]];
+    }
+    return sorted;
+}
+
+function str(Input) {
+	return JSON.stringify(Input);
+};
+
+function estr(Input) {
+	var special = Input.replace(/(\[)/g, "\\\[");
+	special = special.replace(/(\])/g, "\\\]");
+	special = special.replace(/(\{)/g, "\\\{");
+	special = special.replace(/(\})/g, "\\\}");
+	special = special.replace(/(\.)/g, "\\\.");
+	return special;
+};
+
+function prs(Input) {
+  return JSON.parse(Input);
+};
+
+function word(Input) {
+	var newword = ""
+	if (typeof Input === "boolean") {
+		if (Input === false) {
+			newword = "False";
+		} else if (Input === true) {
+			newword = "True";
+		} 
+	}
+	return newword;
+};
