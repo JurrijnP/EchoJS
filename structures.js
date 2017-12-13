4894dec8d8522d0b5e3d9db4403199d92e9fa9bd
JurrijnP <slubadog@gmail.com>
2017-12-13 21:18:28 +0100

    Create structures.js

---
+++ structures.js
@@ -0,0 +1,19 @@
+var newUser = {
+    "Name": "",
+    "Level": 1,
+    "Cash": 1000,
+    "Exp": 0,
+    "totalExp": 0,
+    "Games": {},
+    "Cooldowns": {
+        "Battleships": {},
+        "Heist": {
+            "PreviousPayDay": 0,
+            "PreviousVault": 0
+        }
+    },
+    "Bonuses": {
+        "BattleShips": [],
+        "Heist": []
+    }
+};
